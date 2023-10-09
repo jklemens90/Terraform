35c2e3a78be8ca37bd4a5218a77391dec0bbbd7f
jklemens90 <95970840+jklemens90@users.noreply.github.com>
2023-10-09 19:08:56 -0400

    Delete Three Tier Architecture directory

--- Three Tier Architecture/igw.tf
+++
@@ -1,9 +0,0 @@
-#create an internet gateway and attach it to the VPC so it can connect to the public internet
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "igw"
-  }
-}
