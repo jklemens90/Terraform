d80b8e76458ff1464e7e9fd9bf72c6f18b04028f
jklemens90 <95970840+jklemens90@users.noreply.github.com>
2023-10-14 07:45:05 -0400

    Add files via upload

---
+++ Three Tier Architecture/IAM.tf
@@ -0,0 +1,1 @@
+#IAM for Active Directory
---
+++ Three Tier Architecture/S3.tf
@@ -0,0 +1,4 @@
+#create s3 bucket for logging to store load balancer access logs and misc. files
+#add a policy to send files that are not accessed for 30 days to S3 glacier
+
+
